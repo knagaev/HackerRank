345fe8289dcc6c57d5475f43cbc4270a43dd3031
Konstantin Nagaev <knagaev@users.noreply.github.com>
2020-11-22 23:14:15 +0300

    Create Absolute Permutation.cpp

---
+++ Algorithms/Implementation/Absolute Permutation.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the absolutePermutation function below.
+vector<int> absolutePermutation(int n, int k) 
+{
+    if (k == 0)
+    {
+        vector<int> result(n);
+        iota(begin(result), end(result), 1);
+        return result;
+    }
+
+    if (n % (2 * k) == 0)
+    {
+        vector<int> result(n);
+        //generate(begin(result), end(result), [n, i = n / 2]() mutable { ++i; if (i > n) i = 1; return i; });
+        for (int i = 0, j = 1; i < n; ++i)
+        {
+            result[i] = i + 1 + k * (i / k % 2 * (-2) + 1);
+        }
+        return result;
+    }
+    else
+        return vector<int> {-1};
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string nk_temp;
+        getline(cin, nk_temp);
+
+        vector<string> nk = split_string(nk_temp);
+
+        int n = stoi(nk[0]);
+
+        int k = stoi(nk[1]);
+
+        vector<int> result = absolutePermutation(n, k);
+
+        for (int i = 0; i < result.size(); i++) {
+            fout << result[i];
+
+            if (i != result.size() - 1) {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
