4cb22d2527c82c3aa1438fadde6fbe7abc4b23ad
Konstantin Nagaev <knagaev@users.noreply.github.com>
2021-04-01 13:52:50 +0300

    Create Minimum Time Required.cpp

---
+++ Interview Preparation Kit/Search/Minimum Time Required.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the minTime function below.
+long minTime(vector<long> machines, long goal) 
+{
+    auto maxPeriod = *max_element(machines.begin(), machines.end());
+
+    long leftTime = 0;
+    auto rightTime = goal * maxPeriod;
+
+    while (1)
+    {
+        if (leftTime >= rightTime) return rightTime;
+        long day = (rightTime - leftTime) / 2 + leftTime;
+
+        long produced = 0;
+        bool reach = false;
+        for (const auto& m : machines)
+        {
+            produced += day / m;
+            if (produced >= goal)
+            {
+                reach = true;
+                break;
+            }
+        }
+        if (reach)
+        {
+            rightTime = day;
+        }
+        else
+            leftTime = day + 1;
+    }
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nGoal_temp;
+    getline(cin, nGoal_temp);
+
+    vector<string> nGoal = split_string(nGoal_temp);
+
+    int n = stoi(nGoal[0]);
+
+    long goal = stol(nGoal[1]);
+
+    string machines_temp_temp;
+    getline(cin, machines_temp_temp);
+
+    vector<string> machines_temp = split_string(machines_temp_temp);
+
+    vector<long> machines(n);
+
+    for (int i = 0; i < n; i++) {
+        long machines_item = stol(machines_temp[i]);
+
+        machines[i] = machines_item;
+    }
+
+    long ans = minTime(machines, goal);
+
+    fout << ans << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
