7a122c1b0bf47195fb2b13d8384a9c4733f6fa21
Konstantin Nagaev <knagaev@users.noreply.github.com>
2021-04-01 13:52:24 +0300

    Create Triple sum.cpp

---
+++ Interview Preparation Kit/Search/Triple sum.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the triplets function below.
+long triplets(vector<int> a, vector<int> b, vector<int> c)
+{
+    long triplets = 0;
+
+    set<int> sa(a.begin(), a.end());
+    set<int> sb(b.begin(), b.end());
+    set<int> sc(c.begin(), c.end());
+
+    vector<int> va(sa.begin(), sa.end());
+    vector<int> vb(sb.begin(), sb.end());
+    vector<int> vc(sc.begin(), sc.end());
+
+    auto ub_va = va.begin();
+    auto ub_vc = vc.begin();
+
+    for (const auto& vbi : vb)
+    {
+        auto new_ub_va = upper_bound(ub_va, va.end(), vbi);
+        auto new_ub_vc = upper_bound(ub_vc, vc.end(), vbi);
+
+        if (new_ub_va == va.begin() || new_ub_vc == vc.begin()) continue;
+
+        triplets += distance(va.begin(), new_ub_va) * distance(vc.begin(), new_ub_vc);
+        //cout << vai << " " << *itb << " " << *ub_vc << " : " << dst_vc << endl;
+
+        ub_va = new_ub_va;
+        ub_vc = new_ub_vc;
+    }
+
+    return triplets;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string lenaLenbLenc_temp;
+    getline(cin, lenaLenbLenc_temp);
+
+    vector<string> lenaLenbLenc = split_string(lenaLenbLenc_temp);
+
+    int lena = stoi(lenaLenbLenc[0]);
+
+    int lenb = stoi(lenaLenbLenc[1]);
+
+    int lenc = stoi(lenaLenbLenc[2]);
+
+    string arra_temp_temp;
+    getline(cin, arra_temp_temp);
+
+    vector<string> arra_temp = split_string(arra_temp_temp);
+
+    vector<int> arra(lena);
+
+    for (int i = 0; i < lena; i++) {
+        int arra_item = stoi(arra_temp[i]);
+
+        arra[i] = arra_item;
+    }
+
+    string arrb_temp_temp;
+    getline(cin, arrb_temp_temp);
+
+    vector<string> arrb_temp = split_string(arrb_temp_temp);
+
+    vector<int> arrb(lenb);
+
+    for (int i = 0; i < lenb; i++) {
+        int arrb_item = stoi(arrb_temp[i]);
+
+        arrb[i] = arrb_item;
+    }
+
+    string arrc_temp_temp;
+    getline(cin, arrc_temp_temp);
+
+    vector<string> arrc_temp = split_string(arrc_temp_temp);
+
+    vector<int> arrc(lenc);
+
+    for (int i = 0; i < lenc; i++) {
+        int arrc_item = stoi(arrc_temp[i]);
+
+        arrc[i] = arrc_item;
+    }
+
+    long ans = triplets(arra, arrb, arrc);
+
+    fout << ans << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
